5495fa780ed0dff704c0c9feeaf4f51943403fe3
Manish Devana <manishdevana95@gmail.com>
2018-01-11 01:00:59 +0000

    added updated time reverse model

--- ray_tracing.py
+++ ray_tracing.py
@@ -25,6 +25,7 @@ def instructions():
     Print instructions
     """
 
+
 def wavenumbers(lam_k, lam_l, lam_m):
     """
     Returns wavenumbers from wavelengths
@@ -57,6 +58,14 @@ def cgy(N2, w0, l, kh, m):
     return np.squeeze(((N2 - w0**2)/(w0*(kh**2 + m**2)))*l)
 
 
+def EoZ(N2, w0, f, ):
+    """
+    Wave ray energy when variations can only occur in the vertical (i.e. N2 and
+    flow only vary with depth not horizontally) - Olbers 1981
+    """
+    Ez = np.squeeze((w0**2 * (N2 - f**2))
+                    / ((w0**2 - f**2)**(3 / 2) * (N2 - w0**2)**(1 / 2)))
+    return Ez
 
 
 def make_segments(x, y):
@@ -359,7 +368,142 @@ self.z_ray[-1], duration, tstep, bottom)
 
         plt.gca().invert_yaxis()
 
+    def backward3d(self, duration=24, tstep=5, steady_state=True,status=2,
+                   seafloor=4000, print_run_report=False, updates=False):
+        """
+        ADD DOCS FOR THIS!!
+        Copy of back3d but with depth varying intrinsic frequency and wave
+        action added.
 
+        """
+
+        # Set up model run
+        tstep = float(tstep)
+        duration = duration * 60 * 60  # Convert to seconds
+        time = np.arange(0, duration, tstep)  # time grid for model run
+        status = status*60*60
+        f = gsw.f(self.lat)
+
+        # Run model
+        while steady_state:
+            self.x_all = []
+            self.y_all = []
+            self.z_all = []
+            self.m_all = []
+            self.w0_all = []
+            self.E_all = []
+            self.Ac_all = []
+            z = self.z_init[:]
+            x = self.x_init[:]
+            y = self.y_init[:]
+            w0 = self.w0_init[:]
+            m = self.m_init[:]
+            k = self.k_init[:]
+            l = self.l_init[:]
+            kh = self.kh_init[:]
+
+
+
+            bottom = 'NO'
+            for t in range(len(time)):
+                idx = np.nanargmin(np.abs(z - self.flow_grid))
+                idx2 = np.nanargmin(np.abs(z - self.N2_grid))
+
+                # X group step
+                x = x - (cgx(self.N2[idx2], w0, k,\
+                              kh, m) + self.U[idx])*tstep
+
+
+
+                if not np.isfinite(x):
+                    error = self.model_error_message(x, y, z, m, idx, idx2)
+                    print(' X Error' + error)
+                    break
+                # Y group step
+                y = y - (cgy(self.N2[idx2], w0, l,\
+                              kh, m)+ self.V[idx])*tstep
+
+                if not np.isfinite(y):
+                    error = self.model_error_message(x, y, z, m, idx, idx2)
+                    print(' Y Error' + error)
+                    break
+
+                # Z group step
+                z = z + cgz(w0, f, kh, m)*tstep
+
+                if not np.isfinite(z):
+                    error = self.model_error_message(x, y, z, m, idx, idx2)
+                    print(' X Error' + error)
+                    break
+
+
+                if z >= seafloor:
+                    print('Wave ray hit seafloor')
+                    bottom = 'YES'
+                    break
+
+                # vertical wavenumber change
+                m = m - -1 * (k * self.dudz[idx] + l * self.dvdz[idx]) * tstep
+
+                # Change in frequency (doppler shifting)
+                w0 = w0 - (k * self.U[idx] + l * self.V[idx])
+
+                # Wave Energy(z)
+                Ez = EoZ(self.N2[idx2], w0, f)
+
+                # Wave Action
+                Ac = Ez / w0
+
+                # Sometimes z turns to nan if the velocity or buoyancy profiles
+                # are not complete, right now that breaks the model
+                if not np.isfinite(z):
+                    print('Error')
+                    break
+
+                # Store steps
+                self.x_all.append(x)
+                self.y_all.append(y)
+                self.z_all.append(z)
+                self.m_all.append(m)
+                self.w0_all.append(w0)
+                self.E_all.append(Ez)
+                self.Ac_all.append(Ac)
+
+
+                if time[t] % status == 0 and updates:
+                    print("Progress: {} %".format(int(100
+                                                      * time[t] / duration)))
+
+
+            self.x_ray = np.vstack(self.x_all) / 1000  # Convert to Kilometers
+            self.y_ray = np.vstack(self.y_all) / 1000  # Convert to Kilometers
+
+            self.z_ray = np.vstack(self.z_all)
+            self.m_ray = np.vstack(self.m_all)
+            self.w0_ray = np.vstack(self.w0_all)
+            self.E_ray = np.vstack(self.E_all)
+            self.Ac_ray = np.vstack(self.Ac_all)
+            self.bottom = bottom
+
+            if updates:
+                print('Run Complete!')
+            Run_report = '''
+    Ray Tracing Report:
+    -------------------
+    X-Distance: {} km
+    Y-Distance: {} km
+    Z-Distance: {} m
+    Duration: {} seconds
+    Time Step: {} seconds
+    Hit Sea Floor : {}
+    '''.format(self.x_ray[-1], self.y_ray[-1],
+    self.z_ray[-1], duration, tstep, bottom)
+            self.run_report = Run_report
+
+            if print_run_report:
+                print(Run_report)
+            steady_state = False
+            self.seafloor = seafloor
 
 
 
@@ -372,3 +516,38 @@ self.z_ray[-1], duration, tstep, bottom)
         if not fname:
             fname = 'ray_trace.csv'
             np.savetxt(fname)
+
+
+
+def testing():
+    """
+    This is where I load parameters to test functions
+    """
+    
+    # These are parameters taken from the lee wave dissertation project
+    l1 =  -0.000139543
+    k1 = 0.000324448
+    m1 = -0.00976433
+    z0 = 1500
+    w0 = -0.000132755
+    wave1 = wave(k=k1, l=l1, m=m1, w0=w0, z0=z0)
+    
+    
+    # run models
+    duration = 48
+    tstep = 10
+    status = 6 # intervals to give run status
+    wave1.backward3d(duration=duration, tstep=tstep,
+                     status=status, print_run_report=True)
+        
+    
+    plt.figure()
+    plt.plot(wave1.x_ray, wave1.z_ray)
+    plt.gca().invert_yaxis()
+    
+    
+    
+    
+    
+    
+    
